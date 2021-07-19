441634fc22a8f0a1f388f31136ade746b6fd9a0c
Andrew Walker <awalker@ixsystems.com>
2021-07-19 12:06:43 -0500

    ctdb:tools:ctdb - fix catdb_json output
    TDB data may not be null-terminated. Use talloc_strndup() and
    TDB_DATA size to copy data before writing to json object.

--- ctdb/tools/ctdb.c
+++ ctdb/tools/ctdb.c
@@ -2689,6 +2689,24 @@ struct traverse_cb_data_json {
 	TALLOC_CTX *mem_ctx;
 };
 
+static int tdb_entry_to_json(TALLOC_CTX *mem_ctx,
+			     struct json_object *jsobj,
+			     const char *jskey,
+			     TDB_DATA data)
+{
+	char *to_write = NULL;
+	int error;
+
+	to_write = talloc_strndup(mem_ctx, data.dptr, data.dsize);
+	if (to_write == NULL) {
+		errno = ENOMEM;
+		return -1;
+	}
+	error = json_add_string(jsobj, jskey, to_write);
+	TALLOC_FREE(to_write);
+	return error;
+}
+
 static int record_to_json(uint32_t reqid, struct ctdb_ltdb_header *header,
 			  TDB_DATA key, TDB_DATA data, void *private_data)
 {
@@ -2696,6 +2714,7 @@ static int record_to_json(uint32_t reqid, struct ctdb_ltdb_header *header,
 	int error;
 	bool ok;
 	struct json_object tdb_entry;
+	char *to_write = NULL;
 
 	state = talloc_get_type_abort(private_data,
 				      struct traverse_cb_data_json);
@@ -2715,12 +2734,12 @@ static int record_to_json(uint32_t reqid, struct ctdb_ltdb_header *header,
 		return -1;
 	}
 
-	error = json_add_string(&tdb_entry, "key", key.dptr);
+	error = tdb_entry_to_json(state->mem_ctx, &tdb_entry, "key", key);
 	if (error) {
 		goto fail;
 	}
 
-	error = json_add_string(&tdb_entry, "val", data.dptr);
+	error = tdb_entry_to_json(state->mem_ctx, &tdb_entry, "val", data);
 	if (error) {
 		goto fail;
 	}
