fb5c7b538a8701d54d6c2fc179ba7f01e8ec9444
Andrew Walker <awalker@ixsystems.com>
2021-09-20 15:16:19 -0400

    Fixups for Samba 4.15

--- source3/modules/vfs_streams_xattr.c
+++ source3/modules/vfs_streams_xattr.c
@@ -471,11 +471,10 @@ static int streams_xattr_openat(struct vfs_handle_struct *handle,
 	}
 
 	if (set_empty_xattr && (config->xattr_compat_bytes == 0)) {
-		ret = SMB_VFS_SETXATTR(fsp->conn,
-				       smb_fname,
-				       xattr_name,
-				       NULL, 0,
-				       flags & O_EXCL ? XATTR_CREATE : 0);
+		ret = SMB_VFS_FSETXATTR(fsp->base_fsp,
+					xattr_name,
+					NULL, 0,
+					flags & O_EXCL ? XATTR_CREATE : 0);
 		if (ret != 0) {
 			goto fail;
 		}
@@ -1019,9 +1018,6 @@ static int streams_xattr_connect(vfs_handle_struct *handle,
 
         config->xattr_compat_bytes = xattr_compat ? 0 : 1;
 
-	config->max_xattr_size = (size_t)lp_parm_ulonglong(
-		SNUM(handle->conn), "smbd", "max_xattr_size", 65536);
-
 	SMB_VFS_HANDLE_SET_DATA(handle, config,
 				NULL, struct stream_xattr_config,
 				return -1);
@@ -1037,6 +1033,7 @@ static ssize_t streams_xattr_pwrite(vfs_handle_struct *handle,
 		(struct stream_io *)VFS_FETCH_FSP_EXTENSION(handle, fsp);
 	struct ea_struct ea;
 	int ret;
+	struct streams_xattr_config *config = NULL;
 
 	DBG_DEBUG("offset=%jd, size=%zu\n", (intmax_t)offset, n);
 
@@ -1047,6 +1044,9 @@ static ssize_t streams_xattr_pwrite(vfs_handle_struct *handle,
 	if (!streams_xattr_recheck(sio)) {
 		return -1;
 	}
+
+	SMB_VFS_HANDLE_GET_DATA(handle, config, struct streams_xattr_config,
+				return -1);
 
 	if ((offset + n) >= lp_smbd_max_xattr_size(SNUM(handle->conn))) {
 		/*
