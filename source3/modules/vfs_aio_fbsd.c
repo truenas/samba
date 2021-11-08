3118163bcbe06b8bb46380bfd7b48c3eab50ca50
Andrew Walker <awalker@ixsystems.com>
2021-11-08 07:17:10 -0500

    Update copyright in iX modules

--- source3/modules/vfs_aio_fbsd.c
+++ source3/modules/vfs_aio_fbsd.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) iXsystems 2020
+ * Copyright (C) iXsystems 2021
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
