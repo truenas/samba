3aec8bd013f9b9bc731a2dfedef0ca1934a8517c
Andrew Walker <awalker@ixsystems.com>
2021-11-30 09:51:13 -0500

    s3:modules:streams_xattr - return fakefd for O_EMPTY_PATH opens
    O_EMPTY_PATH indicates that we're re-opening a pathref descriptor
    on an ADS. In this case, fsp->fh is already a fake_fd (important
    part is fsp->base_fsp->fh, which is used for xattr calls).

--- source3/modules/vfs_streams_xattr.c
+++ source3/modules/vfs_streams_xattr.c
@@ -355,11 +355,9 @@ static int streams_xattr_openat(struct vfs_handle_struct *handle,
 	SMB_ASSERT(dirfsp == NULL);
 #if 0 /* Logic from TrueNAS samba 4.15... need to investigate whether applies in 4.17 */
 #ifdef O_EMPTY_PATH
-	if (!flags & O_EMPTY_PATH) {
-		SMB_ASSERT(fsp_get_pathref_fd(dirfsp) == AT_FDCWD);
+	if (flags & O_EMPTY_PATH) {
+		return vfs_fake_fd();
 	}
-#else
-	SMB_ASSERT(fsp_get_pathref_fd(dirfsp) == AT_FDCWD);
 #endif
 #endif 
 	fsp->fsp_flags.have_proc_fds = fsp->conn->have_proc_fds;
