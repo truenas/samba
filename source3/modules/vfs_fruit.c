9d83b00cc69625907b2c372e2e139de11a7b343e
Andrew Walker <awalker@ixsystems.com>
2022-02-02 17:28:41 -0500

    Add handling for cases where AFP_Resource is in file
    Since we're basing TrueNAS 13 on this repo and SCALE
    now supports legacy AFP compatibility by setting
    AFP resource in a separate file, allow unlink in this case.

--- source3/modules/vfs_fruit.c
+++ source3/modules/vfs_fruit.c
@@ -2259,8 +2259,11 @@ static int fruit_unlinkat(vfs_handle_struct *handle,
 	 * delete_all_streams() can't remove 0 byte resource fork
 	 * streams, so we have to cleanup this here.
 	 */
-#if 0
-/* Remove resource stream - skip since we're deleting base file and only streams_xattr is supported. */
+	if ((config->rsrc == FRUIT_RSRC_STREAM) &&
+	    (smb_fname->flags & SMB_FILENAME_POSIX_PATH)) {
+		goto out;
+	}
+
 	rsrc_smb_fname = synthetic_smb_fname(talloc_tos(),
 					     smb_fname->base_name,
 					     AFPRESOURCE_STREAM_NAME,
@@ -2279,8 +2282,8 @@ static int fruit_unlinkat(vfs_handle_struct *handle,
 		return -1;
 	}
 	TALLOC_FREE(rsrc_smb_fname);
-#endif
 
+out:
 	return SMB_VFS_NEXT_UNLINKAT(handle,
 			dirfsp,
 			smb_fname,
