d9d8a9ae34600792e9a163d16df82d9b8bb64fb2
Andrew Walker <awalker@ixsystems.com>
2022-02-02 14:35:22 -0500

    Make socket listen backlog configurable on FreeBSD
    setting this to -1 allows it to be controlled via sysctl

--- source3/winbindd/winbindd.c
+++ source3/winbindd/winbindd.c
@@ -1040,7 +1040,7 @@ static bool winbindd_setup_listeners(void)
 	if (pub_state->fd == -1) {
 		goto failed;
 	}
-	rc = listen(pub_state->fd, 5);
+	rc = listen(pub_state->fd, DEFAULT_LISTEN_BACKLOG);
 	if (rc < 0) {
 		goto failed;
 	}
@@ -1072,7 +1072,7 @@ static bool winbindd_setup_listeners(void)
 	if (priv_state->fd == -1) {
 		goto failed;
 	}
-	rc = listen(priv_state->fd, 5);
+	rc = listen(priv_state->fd, DEFAULT_LISTEN_BACKLOG);
 	if (rc < 0) {
 		goto failed;
 	}
