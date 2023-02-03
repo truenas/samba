118801c65e0b105ff9a6f53998f0074fcff5c552
Andrew Walker <awalker@ixsystems.com>
2023-02-03 12:30:15 -0800

    lib/tevent/testsuite - add FreeBSD AIO tests (#236)
    Add basic torture tests for FreeBSD kqueue-based
    AIO.

--- source4/torture/local/local.c
+++ source4/torture/local/local.c
@@ -63,6 +63,7 @@
 	torture_local_string_case,
 	torture_local_util_unistr,
 	torture_local_event,
+	torture_local_event_aio,
 	torture_local_tevent_req,
 	torture_local_torture,
 	torture_local_dbspeed,
