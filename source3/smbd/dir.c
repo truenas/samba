14f409a004a60ead85b066b28a4a9519410c05f8
Andrew Walker <awalker@ixsystems.com>
2025-06-26 14:09:39 -0600

    Fix

--- source3/smbd/dir.c
+++ source3/smbd/dir.c
@@ -955,7 +955,7 @@ bool is_visible_fsp(struct files_struct *fsp)
 			DBG_ERR("%s: open flags: 0x%08x\n",
 				fsp_str_dbg(fsp), open_flags);
 
-			return open_flags & O_RDONLY;
+			return open_flags & O_RDONLY ? true : false;
 		}
 	}
 
