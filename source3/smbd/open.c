a798d1cd6d8be2530798b971660f22eb613ef5be
Andrew Walker <awalker@ixsystems.com>
2025-07-15 10:13:39 -0600

    Remove unnecessary call to set archive bit
    This commit does two things:
    1. It removes one unnecessary call to set the archive bit when
       creating new files. ZFS automatically does this for us.
    
    2. It ensures that "kernel dosmodes" is always set by vfs_ixnas
       if the module is enabled with dosmode support.

--- source3/smbd/open.c
+++ source3/smbd/open.c
@@ -3606,7 +3606,8 @@ static void possibly_set_archive(struct connection_struct *conn,
 	bool set_archive = false;
 	int ret;
 
-	if (info == FILE_WAS_OPENED) {
+	// If kernel doesmodes are enabled then ARCHIVE is set by the FS
+	if ((info == FILE_WAS_OPENED) || lp_kernel_dosmodes(SNUM(conn))) {
 		return;
 	}
 
@@ -3614,8 +3615,6 @@ static void possibly_set_archive(struct connection_struct *conn,
 	if ((info == FILE_WAS_OVERWRITTEN && lp_map_archive(SNUM(conn)))) {
 		set_archive = true;
 	} else if (lp_store_dos_attributes(SNUM(conn))) {
-		set_archive = true;
-	} else if (lp_kernel_dosmodes(SNUM(conn))) {
 		set_archive = true;
 	}
 	if (!set_archive) {
