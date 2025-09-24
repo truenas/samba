4652dd588524e7dded3d4b6ff7199cea98468c88
Andrew Walker <awalker@ixsystems.com>
2025-09-24 11:29:13 -0500

    NAS-137691 / Properly initialize mangle for spotlight RPC server (#481)
    This commit initializes the mangle function pointers (global
    state) in the forked RPC server that handles spotlight requests.

--- source3/rpc_server/rpcd_mdssvc.c
+++ source3/rpc_server/rpcd_mdssvc.c
@@ -21,6 +21,7 @@
 #include "rpc_worker.h"
 #include "librpc/gen_ndr/ndr_mdssvc.h"
 #include "librpc/gen_ndr/ndr_mdssvc_scompat.h"
+#include "source3/smbd/proto.h"
 
 static size_t mdssvc_interfaces(
 	const struct ndr_interface_table ***pifaces,
@@ -44,6 +45,7 @@ static NTSTATUS mdssvc_servers(
 	bool ok;
 
 	lp_load_with_shares(get_dyn_CONFIGFILE());
+	mangle_reset_cache();
 
 	ok = posix_locking_init(false);
 	if (!ok) {
