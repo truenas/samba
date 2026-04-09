a3f3c5dd2b5b60730f38d8496ad2526fb1fbdec4
Andrew Walker <awalker@ixsystems.com>
2026-04-09 13:14:18 -0500

    NAS-140631 / Add RESOLVE_NO_XDEV handling (#535)
    This commit adds a non-default configurable option to
    vfs_shadow_copy_zfs to disable dataset traversal by setting
    the RESOLVE_NO_XDEV openat2 resolve flag for paths that are
    not snapdir-related. The business logic for determining
    whether to set this is inside the shadow copy module because
    that's the place where we determine whether a path is a
    snapshot. In the future we can probably consolidate into
    vfs_zfs_core module.

--- source3/include/vfs.h
+++ source3/include/vfs.h
@@ -719,6 +719,7 @@ typedef struct files_struct {
 #define TCON_FLAG_STREAMS_XATTR		0x02
 #define TCON_FLAG_STREAMS_FILE		0x04
 #define TCON_FLAG_TRUENAS_ABE		0x10
+#define TCON_FLAG_NOXDEV		0x20
 
 struct vuid_cache_entry {
 	struct auth_session_info *session_info;
